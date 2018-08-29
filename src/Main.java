4bd6ec0ec2d264525a6f9473774f80bcc00eabd7
NatMannn <59160953@go.buu.ac.th>
2018-08-29 18:56:38 +0700

    Lab3_1 First Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String input = JOptionPane.showInputDialog("ใส่ตัวเลข 1 ตัวนะ","0");
+        JOptionPane.showMessageDialog(null,input,"title",JOptionPane.INFORMATION_MESSAGE);
+        int ans = JOptionPane.showConfirmDialog(null,"รักหรือเปล่าจ๊ะ",
+                "ถาามเธอไงจะใครล่ะ",JOptionPane.YES_NO_OPTION);
+        if(ans==JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"รักนะจุ๊บๆอิอิคิคิ");
+        }else{
+            JOptionPane.showMessageDialog(null,"จำไว้เลยนะเธอ");
+        }
+    }
+}
